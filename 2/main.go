60a3c69997f9177d1fe9ab5eabce0a6ad598c2d4
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2023-12-02 19:06:56 +0100

    Refactor day 2

--- 2/main.go
+++ 2/main.go
@@ -16,28 +16,21 @@ func Puzzle(input string) int {
 		"blue":  14,
 	}
 
-	var sum int
-	scanner := bufio.NewScanner(strings.NewReader(input))
-	for scanner.Scan() {
-		line := strings.ReplaceAll(scanner.Text(), "Game ", "")
-		parts := strings.Split(line, ": ")
-		id, err := strconv.Atoi(parts[0])
-		if err != nil {
-			fmt.Printf("error occurred: %v\n", err)
-		}
+	games := parse(input)
+	sum := 0
 
+	for id, sets := range games {
 		possible := true
-		sets := strings.Split(parts[1], "; ")
-		for _, set := range sets {
-			for _, s := range strings.Split(set, ", ") {
-				p := strings.Split(s, " ")
-				v, err := strconv.Atoi(p[0])
-				if err != nil {
-					fmt.Printf("error occurred: %v\n", err)
-				}
-				if v > rules[p[1]] {
+		for _, subset := range sets {
+			for color, value := range subset {
+				if value > rules[color] {
 					possible = false
+					break
 				}
+			}
+
+			if !possible {
+				break
 			}
 		}
 
@@ -46,48 +39,73 @@ func Puzzle(input string) int {
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("error occurred: %v\n", err)
-	}
-
 	return sum
 }
 
 func PuzzleTwo(input string) int {
-	var power int
-	scanner := bufio.NewScanner(strings.NewReader(input))
-	for scanner.Scan() {
-		line := strings.ReplaceAll(scanner.Text(), "Game ", "")
-		parts := strings.Split(line, ": ")
+	games := parse(input)
+	power := 0
 
-		max := map[string]int{
+	for _, sets := range games {
+		maximums := map[string]int{
 			"red":   0,
 			"green": 0,
 			"blue":  0,
 		}
-		sets := strings.Split(parts[1], "; ")
-		for _, set := range sets {
-			for _, s := range strings.Split(set, ", ") {
-				p := strings.Split(s, " ")
-				v, err := strconv.Atoi(p[0])
-				if err != nil {
-					fmt.Printf("error occurred: %v\n", err)
-				}
-				n, ok := max[p[1]]
-				if !ok || v > n {
-					max[p[1]] = v
+
+		for _, subset := range sets {
+			for color, value := range subset {
+				n, ok := maximums[color]
+				if !ok || value > n {
+					maximums[color] = value
 				}
 			}
 		}
+		power += maximums["red"] * maximums["green"] * maximums["blue"]
+	}
 
-		power += max["red"] * max["green"] * max["blue"]
+	return power
+}
+
+type Game map[string]int
+type Games map[int][]Game
+
+func parse(input string) Games {
+	games := Games{}
+
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	for scanner.Scan() {
+		parseLine(scanner.Text(), games)
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("error occurred: %v\n", err)
 	}
 
-	return power
+	return games
+}
+
+func parseLine(line string, games Games) {
+	line = strings.ReplaceAll(line, "Game ", "")
+	parts := strings.Split(line, ": ")
+
+	id, _ := strconv.Atoi(parts[0])
+	sets := strings.Split(parts[1], "; ")
+
+	games[id] = []Game{}
+	for _, set := range sets {
+		game := map[string]int{}
+		parseSet(set, game)
+		games[id] = append(games[id], game)
+	}
+}
+
+func parseSet(set string, game Game) {
+	for _, subset := range strings.Split(set, ", ") {
+		p := strings.Split(subset, " ")
+		v, _ := strconv.Atoi(p[0])
+		game[p[1]] = v
+	}
 }
 
 //go:embed *.txt
